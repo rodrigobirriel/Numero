58c5e7160a0a5c339b3de1ee230ebb965d240073
Rodrigo <rbirriel@altimetrik.com>
2022-01-25 01:48:49 -0300

    final comment

--- Numero/Converter.swift
+++ Numero/Converter.swift
@@ -25,7 +25,7 @@
 /// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
-
+// last comment
 class Converter {
   func convert(_ number: Int) -> String {
     return String(repeating: "I", count: number)
